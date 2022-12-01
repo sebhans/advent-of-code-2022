e3cff30d4c6848398522c7c23b83fd25a2e5ef06
Sebastian Hans <ich@sebastian-hans.de>
2022-12-01 13:25:07 +0100

    solve day 1, part 2

--- day1.exs
+++ day1.exs
@@ -2,7 +2,10 @@ chomp = fn s -> String.replace_suffix(s, "\n", "") end
 input = chomp.(File.read!("input/day1.txt"))
 input_per_elf = String.split(input, "\n\n")
 calories_list = Enum.map(input_per_elf, fn s -> Enum.map(String.split(s, "\n"), &String.to_integer/1) end)
-sum = fn xs -> Enum.reduce(xs, &+/2) end
+calories_sums = Enum.map(calories_list, &Enum.sum/1)
 max = fn a, b -> if a >= b, do: a, else: b end
-most_calories = Enum.reduce(Enum.map(calories_list, sum), max)
+most_calories = Enum.reduce(calories_sums, max)
 IO.puts(most_calories)
+
+top3_sum = Enum.sum(Enum.take(Enum.sort(calories_sums, &(&1 >= &2)), 3))
+IO.puts(top3_sum)
