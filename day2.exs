e4e3a5d14e3e7f1a1f9d87092b4406a8cbd55413
Sebastian Hans <ich@sebastian-hans.de>
2022-12-02 20:06:55 +0100

    solve day 2, part 2

--- day2.exs
+++ day2.exs
@@ -16,3 +16,20 @@ score = %{
   {:scissors, :scissors} => 6
 }
 IO.puts(Enum.sum(Enum.map(guessed_rounds, &(score[&1]))))
+
+strategy_guide_real_key = %{"A" => :rock, "B" => :paper, "C" => :scissors, "X" => :lose, "Y" => :draw, "Z" => :win}
+my_move = %{
+  {:rock, :lose} => :scissors,
+  {:rock, :draw} => :rock,
+  {:rock, :win} => :paper,
+  {:paper, :lose} => :rock,
+  {:paper, :draw} => :paper,
+  {:paper, :win} => :scissors,
+  {:scissors, :lose} => :paper,
+  {:scissors, :draw} => :scissors,
+  {:scissors, :win} => :rock
+}
+figure_out_choices = fn {opponent, result} -> {opponent, my_move[{opponent, result}]} end
+real_rounds = Enum.map(Enum.map(input_rounds, interpret.(strategy_guide_real_key)), figure_out_choices)
+
+IO.puts(Enum.sum(Enum.map(real_rounds, &(score[&1]))))
