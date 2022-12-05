4321552ccb70f9f417284b5f6f146877669a40d6
Sebastian Hans <Hans.Sebastian@swm.de>
2022-12-05 08:42:15 +0100

    solve day 4, part 2

--- day4.exs
+++ day4.exs
@@ -5,3 +5,6 @@ assignments = Enum.map(Regex.scan(~r/([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)/, input
   fn section_strings -> Enum.map(section_strings, &String.to_integer/1) end)
 redundant_assignments = Enum.filter(assignments, fn [start1, end1, start2, end2] -> (start1<=start2 and end1>=end2) or (start2<=start1 and end2>=end1) end)
 IO.puts(length(redundant_assignments))
+
+overlapping_assignments = Enum.filter(assignments, fn [start1, end1, start2, end2] -> (start1<=start2 and end1>=start2) or (start2<=start1 and end2>=start1) end)
+IO.puts(length(overlapping_assignments))
