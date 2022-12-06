3b002809fbe37c9892ef4d7167d1d5644df948f3
Sebastian Hans <Hans.Sebastian@swm.de>
2022-12-06 11:29:46 +0100

    solve day 6, part 2

--- day6.exs
+++ day6.exs
@@ -2,9 +2,12 @@
 chomp = fn s -> String.replace_suffix(s, "\n", "") end
 input = chomp.(File.read!("input/day6.txt"))
 second = fn tuple -> elem(tuple, 1) end
-marker_start = Stream.zip(Stream.chunk_every(String.to_charlist(input), 4, 1), Stream.iterate(1, &(&1 + 1)))
-|> Stream.filter(&(Enum.uniq(elem(&1, 0)) == elem(&1, 0)))
-|> Enum.take(1)
-|> Enum.at(0)
-|> second.()
-IO.puts(marker_start + 3)
+find_marker = fn marker_length ->
+  Stream.zip(Stream.chunk_every(String.to_charlist(input), marker_length, 1), Stream.iterate(1, &(&1 + 1)))
+  |> Stream.filter(&(Enum.uniq(elem(&1, 0)) == elem(&1, 0)))
+  |> Enum.at(0)
+  |> second.()
+  |> Kernel.+(marker_length - 1)
+end
+IO.puts(find_marker.(4))
+IO.puts(find_marker.(14))
