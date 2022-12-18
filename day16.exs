d17092fea5f6a0fde213b17b273aa3be23baac1c
Sebastian Hans <ich@sebastian-hans.de>
2022-12-18 20:09:38 +0100

    refactor solution to day 16

--- day16.exs
+++ day16.exs
@@ -8,12 +8,8 @@ defmodule Valves do
     |> Enum.map(fn [name, rate, tunnels] -> {name, String.to_integer(rate), String.split(tunnels, ", ")} end)
     |> Enum.reduce(%{}, fn valve, valves -> Map.put(valves, elem(valve, 0), valve) end)
   end
-end
 
-valves = Valves.parse(input)
-
-Enum.reduce(1..30, [{"AA", MapSet.new(), 0, 0}], fn _, paths ->
-  Enum.flat_map(paths, fn {position, open, total_rate, released} ->
+  def move(valves), do: fn {position, open, total_rate, released} ->
     {name, rate, tunnels} = Map.get(valves, position)
     open? = MapSet.member?(open, name)
     (if (not open?) and rate > 0 do
@@ -22,7 +18,14 @@ Enum.reduce(1..30, [{"AA", MapSet.new(), 0, 0}], fn _, paths ->
       []
     end) ++
       Enum.map(tunnels, fn tunnel -> {tunnel, open, total_rate, released + total_rate} end)
-  end)
+  end
+end
+
+valves = Valves.parse(input)
+move = Valves.move(valves)
+
+Enum.reduce(1..30, [{"AA", MapSet.new(), 0, 0}], fn _, paths ->
+  Enum.flat_map(paths, move)
   |> Enum.sort_by(fn {position, _, total_rate, released} -> {position, total_rate, released} end, &>=/2)
   |> Enum.dedup_by(fn {position, _, total_rate, _} -> {position, total_rate} end)
 end)
