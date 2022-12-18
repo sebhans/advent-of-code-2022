a0313dfb1c3dda97d6d2f64a4244623db0efcaed
Sebastian Hans <ich@sebastian-hans.de>
2022-12-18 18:21:04 +0100

    solve day 16, part 1

---
+++ day16.exs
@@ -0,0 +1,31 @@
+#!/usr/bin/env elixir
+chomp = fn s -> String.replace_suffix(s, "\n", "") end
+input = chomp.(File.read!("input/day16.txt"))
+
+defmodule Valves do
+  def parse(input) do
+    Regex.scan(~r/Valve ([A-Z]+) has flow rate=([0-9]+); tunnels? leads? to valves? ([A-Z, ]+)/, input, capture: :all_but_first)
+    |> Enum.map(fn [name, rate, tunnels] -> {name, String.to_integer(rate), String.split(tunnels, ", ")} end)
+    |> Enum.reduce(%{}, fn valve, valves -> Map.put(valves, elem(valve, 0), valve) end)
+  end
+end
+
+valves = Valves.parse(input)
+
+Enum.reduce(1..30, [{"AA", MapSet.new(), 0, 0}], fn _, paths ->
+  Enum.flat_map(paths, fn {position, open, total_rate, released} ->
+    {name, rate, tunnels} = Map.get(valves, position)
+    open? = MapSet.member?(open, name)
+    (if (not open?) and rate > 0 do
+      [{position, MapSet.put(open, name), total_rate + rate, released + total_rate}]
+    else
+      []
+    end) ++
+      Enum.map(tunnels, fn tunnel -> {tunnel, open, total_rate, released + total_rate} end)
+  end)
+  |> Enum.sort_by(fn {position, _, total_rate, released} -> {position, total_rate, released} end, &>=/2)
+  |> Enum.dedup_by(fn {position, _, total_rate, _} -> {position, total_rate} end)
+end)
+|> Enum.max_by(fn {_, _, _, released} -> released end)
+|> elem(3)
+|> IO.inspect
